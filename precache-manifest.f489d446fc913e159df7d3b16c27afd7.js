712250093caaf6b9b2b989a8a60a4b36842cdbe4
OM JHA <rewantom@gmail.com>
2024-10-27 19:04:56 +0530

    Updates

---
+++ precache-manifest.f489d446fc913e159df7d3b16c27afd7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d22bb7e7e0e98875950d94d0a889ce8",
+    "url": "/OM_JHA_EVENTSPOTLITE/index.html"
+  },
+  {
+    "revision": "41e3579101013b456e4e",
+    "url": "/OM_JHA_EVENTSPOTLITE/static/css/main.8830f819.chunk.css"
+  },
+  {
+    "revision": "8d2eca129c9fad5616fc",
+    "url": "/OM_JHA_EVENTSPOTLITE/static/js/2.9cc06df2.chunk.js"
+  },
+  {
+    "revision": "41e3579101013b456e4e",
+    "url": "/OM_JHA_EVENTSPOTLITE/static/js/main.c4722a5d.chunk.js"
+  },
+  {
+    "revision": "9323b43a74def001a33d",
+    "url": "/OM_JHA_EVENTSPOTLITE/static/js/runtime~main.c7ebac54.js"
+  }
+]);
